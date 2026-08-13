8d2bc907a1e253162150565bc19b7886604f26d6
sdngegerkedungadem <sdngegerkedungadem@gmail.com>
2026-08-13 11:52:48 +0700

    Configure GAS API bridge

---
+++ assets/js/config.js
@@ -0,0 +1,4 @@
+window.HABIT_TRACKER_CONFIG = Object.freeze({
+  bridgeUrl: 'https://script.google.com/macros/s/AKfycbxiyRjs0znPBh5V66Az0RuoO4oRzTvtaNNr-BswlfBmk2LSvzew9W5ApkQV87LuxxNFrg/exec?bridge=1',
+  requestTimeoutMs: 70000
+});
